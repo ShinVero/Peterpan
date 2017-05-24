8e4d6dbbbb992474969692f0be7263d44c28a05c
MSI GP62 <MSI GP62@SHINVERO>
2017-05-25 01:38:24 +0700

    Biaya, Navigator

---
+++ Peterpan II/Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Btn_Clear = New System.Windows.Forms.Button()
+        Me.Btn_Save = New System.Windows.Forms.Button()
+        Me.Btn_Proses = New System.Windows.Forms.Button()
+        Me.Txt_Biaya = New System.Windows.Forms.TextBox()
+        Me.Txt_No_Transaksi = New System.Windows.Forms.TextBox()
+        Me.label7 = New System.Windows.Forms.Label()
+        Me.label6 = New System.Windows.Forms.Label()
+        Me.label5 = New System.Windows.Forms.Label()
+        Me.label4 = New System.Windows.Forms.Label()
+        Me.label3 = New System.Windows.Forms.Label()
+        Me.Txt_No_Parkir = New System.Windows.Forms.TextBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.Txt_JamMasuk = New System.Windows.Forms.TextBox()
+        Me.Txt_JamKeluar = New System.Windows.Forms.TextBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Btn_Clear
+        '
+        Me.Btn_Clear.Enabled = False
+        Me.Btn_Clear.Location = New System.Drawing.Point(235, 192)
+        Me.Btn_Clear.Name = "Btn_Clear"
+        Me.Btn_Clear.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Clear.TabIndex = 31
+        Me.Btn_Clear.Text = "Clear"
+        Me.Btn_Clear.UseVisualStyleBackColor = True
+        '
+        'Btn_Save
+        '
+        Me.Btn_Save.Enabled = False
+        Me.Btn_Save.Location = New System.Drawing.Point(122, 192)
+        Me.Btn_Save.Name = "Btn_Save"
+        Me.Btn_Save.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Save.TabIndex = 30
+        Me.Btn_Save.Text = "Save"
+        Me.Btn_Save.UseVisualStyleBackColor = True
+        '
+        'Btn_Proses
+        '
+        Me.Btn_Proses.Location = New System.Drawing.Point(16, 192)
+        Me.Btn_Proses.Name = "Btn_Proses"
+        Me.Btn_Proses.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_Proses.TabIndex = 29
+        Me.Btn_Proses.Text = "Proses"
+        Me.Btn_Proses.UseVisualStyleBackColor = True
+        '
+        'Txt_Biaya
+        '
+        Me.Txt_Biaya.Enabled = False
+        Me.Txt_Biaya.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold)
+        Me.Txt_Biaya.Location = New System.Drawing.Point(10, 31)
+        Me.Txt_Biaya.Multiline = True
+        Me.Txt_Biaya.Name = "Txt_Biaya"
+        Me.Txt_Biaya.Size = New System.Drawing.Size(443, 50)
+        Me.Txt_Biaya.TabIndex = 28
+        Me.Txt_Biaya.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Txt_No_Transaksi
+        '
+        Me.Txt_No_Transaksi.Enabled = False
+        Me.Txt_No_Transaksi.Location = New System.Drawing.Point(124, 98)
+        Me.Txt_No_Transaksi.Name = "Txt_No_Transaksi"
+        Me.Txt_No_Transaksi.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_No_Transaksi.TabIndex = 25
+        '
+        'label7
+        '
+        Me.label7.AutoSize = True
+        Me.label7.Location = New System.Drawing.Point(10, 95)
+        Me.label7.Name = "label7"
+        Me.label7.Size = New System.Drawing.Size(108, 17)
+        Me.label7.TabIndex = 24
+        Me.label7.Text = "No. Transaksi : "
+        '
+        'label6
+        '
+        Me.label6.AutoSize = True
+        Me.label6.Location = New System.Drawing.Point(197, 80)
+        Me.label6.Name = "label6"
+        Me.label6.Size = New System.Drawing.Size(0, 17)
+        Me.label6.TabIndex = 23
+        '
+        'label5
+        '
+        Me.label5.AutoSize = True
+        Me.label5.Location = New System.Drawing.Point(13, 11)
+        Me.label5.Name = "label5"
+        Me.label5.Size = New System.Drawing.Size(92, 17)
+        Me.label5.TabIndex = 22
+        Me.label5.Text = "Biaya Parkir :"
+        '
+        'label4
+        '
+        Me.label4.AutoSize = True
+        Me.label4.Location = New System.Drawing.Point(259, 138)
+        Me.label4.Name = "label4"
+        Me.label4.Size = New System.Drawing.Size(95, 17)
+        Me.label4.TabIndex = 21
+        Me.label4.Text = "Jam_Keluar : "
+        '
+        'label3
+        '
+        Me.label3.AutoSize = True
+        Me.label3.Location = New System.Drawing.Point(10, 140)
+        Me.label3.Name = "label3"
+        Me.label3.Size = New System.Drawing.Size(95, 17)
+        Me.label3.TabIndex = 20
+        Me.label3.Text = "Jam_Masuk : "
+        '
+        'Txt_No_Parkir
+        '
+        Me.Txt_No_Parkir.Location = New System.Drawing.Point(373, 95)
+        Me.Txt_No_Parkir.Name = "Txt_No_Parkir"
+        Me.Txt_No_Parkir.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_No_Parkir.TabIndex = 17
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Location = New System.Drawing.Point(259, 98)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(79, 17)
+        Me.label1.TabIndex = 16
+        Me.label1.Text = "No.Parkir : "
+        '
+        'Txt_JamMasuk
+        '
+        Me.Txt_JamMasuk.Location = New System.Drawing.Point(124, 135)
+        Me.Txt_JamMasuk.Name = "Txt_JamMasuk"
+        Me.Txt_JamMasuk.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_JamMasuk.TabIndex = 26
+        '
+        'Txt_JamKeluar
+        '
+        Me.Txt_JamKeluar.Enabled = False
+        Me.Txt_JamKeluar.Location = New System.Drawing.Point(373, 137)
+        Me.Txt_JamKeluar.Name = "Txt_JamKeluar"
+        Me.Txt_JamKeluar.Size = New System.Drawing.Size(100, 22)
+        Me.Txt_JamKeluar.TabIndex = 27
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(490, 244)
+        Me.Controls.Add(Me.Btn_Clear)
+        Me.Controls.Add(Me.Btn_Save)
+        Me.Controls.Add(Me.Btn_Proses)
+        Me.Controls.Add(Me.Txt_Biaya)
+        Me.Controls.Add(Me.Txt_JamKeluar)
+        Me.Controls.Add(Me.Txt_JamMasuk)
+        Me.Controls.Add(Me.Txt_No_Transaksi)
+        Me.Controls.Add(Me.label7)
+        Me.Controls.Add(Me.label6)
+        Me.Controls.Add(Me.label5)
+        Me.Controls.Add(Me.label4)
+        Me.Controls.Add(Me.label3)
+        Me.Controls.Add(Me.Txt_No_Parkir)
+        Me.Controls.Add(Me.label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents Btn_Clear As Button
+    Private WithEvents Btn_Save As Button
+    Private WithEvents Btn_Proses As Button
+    Private WithEvents Txt_Biaya As TextBox
+    Private WithEvents Txt_No_Transaksi As TextBox
+    Private WithEvents label7 As Label
+    Private WithEvents label6 As Label
+    Private WithEvents label5 As Label
+    Private WithEvents label4 As Label
+    Private WithEvents label3 As Label
+    Private WithEvents Txt_No_Parkir As TextBox
+    Private WithEvents label1 As Label
+    Private WithEvents Txt_JamMasuk As TextBox
+    Private WithEvents Txt_JamKeluar As TextBox
+    Friend WithEvents Timer1 As Timer
+    Public WithEvents Timer2 As Timer
+End Class
---
+++ Peterpan II/Form1.vb
@@ -0,0 +1,130 @@
+﻿Public Class Form1
+    Dim no_transaksi As Integer
+    Dim no_parkir As String = "MU13"
+    Dim PlatPolisi As String
+    Dim jam_masuk As DateTime
+    Dim jam_keluar As DateTime
+    Dim biaya_Parkir As Integer
+    Dim Deposito As Integer = 10000
+    Dim Total As Integer
+    Sub modestandby()
+        Txt_Biaya.Enabled = False
+        Txt_JamMasuk.Enabled = False
+        Txt_No_Transaksi.Enabled = False
+        Txt_No_Parkir.Enabled = True
+        Btn_Proses.Enabled = True
+        Btn_Save.Enabled = False
+        Btn_Clear.Enabled = False
+    End Sub
+
+    Sub modeEdit()
+        Txt_Biaya.Enabled = True
+        Txt_JamMasuk.Enabled = True
+        Txt_No_Transaksi.Enabled = False
+        Txt_No_Parkir.Enabled = True
+        Txt_JamMasuk.Enabled = True
+        Btn_Clear.Enabled = True
+        Btn_Save.Enabled = True
+        Btn_Proses.Enabled = False
+    End Sub
+
+    Private Sub Btn_Proses_Click(sender As Object, e As EventArgs) Handles Btn_Proses.Click
+        'Proses pengambilan data dari database
+
+        For x = 0 To no_parkir.Count - 1
+            If Txt_No_Parkir.Text = "" Then
+                MessageBox.Show("Anda Belum mengisi no parkir")
+                Exit For
+            End If
+            If Txt_No_Parkir.Text = no_parkir Then
+                modeEdit()
+                no_transaksi += 1
+                no_parkir = Txt_No_Parkir.Text
+                Txt_No_Transaksi.Text = no_transaksi
+
+                'Mendapatkan selisih waktu
+                Dim ts As TimeSpan =
+                    (Convert.ToDateTime(Txt_JamKeluar.Text).Subtract(Convert.ToDateTime(Txt_JamMasuk.Text))).Duration()
+                Dim Lama_Parkir As String = [String].Format("{0}", ts.Hours)
+
+                'Pengecekakn Jenis kendaraan
+                If no_parkir.Substring(0, 1) = "M" Then
+                    biaya_Parkir = Lama_Parkir * 2000
+                End If
+
+                'Pengecekan jenis pelanggan
+                If no_parkir.Substring(1, 1) = "L" Then
+                    If Deposito = 0 Then
+                        MessageBox.Show("Anda Sudah tidak memiliki saldo")
+                    ElseIf Deposito > biaya_Parkir Then
+                        Deposito -= biaya_Parkir
+                        MessageBox.Show("Anda Memiliki sisa saldo sebesar : " & Deposito)
+                        biaya_Parkir = 0
+                    Else
+                        biaya_Parkir -= Deposito
+                        Deposito = 0
+                        MessageBox.Show("Anda Memiliki sisa saldo sebesar : " & Deposito)
+                    End If
+                ElseIf no_parkir.Substring(1, 1) = "S" Then
+                    biaya_Parkir = 0
+                End If
+                Txt_Biaya.Text = Format(biaya_Parkir, "##,###")
+                Exit For
+            Else
+                MessageBox.Show("Maaf Data tidak ditemukkan")
+                Txt_No_Parkir.Clear()
+                Exit For
+            End If
+        Next
+
+    End Sub
+
+    Private Sub Btn_Save_Click(sender As Object, e As EventArgs) Handles Btn_Save.Click
+        'proses pembayaran dan penyimpanan data..ketika tombol ini di klik
+        Dim JnsPelanggan As String
+        Dim JnsKendaraan As String
+        If no_parkir.Substring(1, 1) = "L" Then
+            JnsPelanggan = "Langganan"
+        ElseIf no_parkir.Substring(1, 1) = "S" Then
+            JnsPelanggan = "Staff"
+        Else
+            JnsPelanggan = "Umum"
+        End If
+        If no_parkir.Substring(0, 1) = "M" Then
+            JnsKendaraan = "Mobil"
+        End If
+        MessageBox.Show("No Transaksi : " & no_transaksi &
+                        " No Parkir : " & no_parkir &
+                        " Jenis Pelanggan : " & JnsPelanggan &
+                        " Jenis Kendaraan : " & JnsKendaraan &
+                        " Jam Masuk : " & Txt_JamMasuk.Text &
+                        " Jam_Keluar : " & Txt_JamKeluar.Text)
+        'anggap saja palang parkir terbuka
+        modestandby()
+        Txt_Biaya.Clear()
+        Txt_JamMasuk.Clear()
+        Txt_No_Transaksi.Clear()
+        Txt_No_Parkir.Clear()
+    End Sub
+
+    Private Sub Btn_Clear_Click(sender As Object, e As EventArgs) Handles Btn_Clear.Click
+        Txt_Biaya.Clear()
+        Txt_JamMasuk.Clear()
+        Txt_No_Transaksi.Clear()
+        Txt_No_Parkir.Clear()
+        If no_transaksi = 0 Then
+            MessageBox.Show("tidak ada transaksi")
+        Else
+            no_transaksi -= 1
+        End If
+        modestandby()
+    End Sub
+
+    Private Sub Timer1_Tick_1(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Txt_JamKeluar.Text = Format(Now, "hh:mm:ss")
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Txt_JamMasuk.Text = Format("05:00:00")
+    End Sub
+End Class
